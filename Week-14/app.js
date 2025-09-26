863e121e35cf57fc2a1035b40b811c09f0628a7e
Mac <mac@Macs-MacBook-Air.local>
2025-09-26 19:35:20 +0500

    Add Week 14 tasks

---
+++ Week-14/app.js
@@ -0,0 +1,24 @@
+// let n = 5;
+// for (let i=0; i<5; i++){
+//     console.log("hello", i);
+// }
+// console.log("bye!");
+
+
+// let args = process.argv;
+// for (let i=2; i<args.length; i++){
+//     console.log("hello to", args[i]);
+// } 
+// console.log(process.argv);
+
+// const math = require("./math");
+// console.log(math.sum(3,6));
+
+// const fruits = require("./Fruits");
+
+// console.log(fruits);
+
+// import {sum, PI} from "./math.js";
+import { generate } from "random-words";
+console.log(generate());
+// console.log(sum(1,2));
